e2b1285e86461bcf44e109fd48c9cda7146433db
Henry.Liu <167392646+HenryLiukkk@users.noreply.github.com>
2024-12-24 21:26:20 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
   plugins: [
     vue(),
   ],
-  base:'/academic-project-page-template-vue',
+  base:'/FLAIR-LAB',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
