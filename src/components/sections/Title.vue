d1137533d9219e92965dc138beb4f1360f53b805
Henry.Liu <167392646+HenryLiukkk@users.noreply.github.com>
2024-12-24 21:41:31 +0800

    Update Title.vue

--- src/components/sections/Title.vue
+++ src/components/sections/Title.vue
@@ -7,7 +7,7 @@ import { Document, Files, MagicStick, Picture, DataAnalysis, Film } from '@eleme
 const logo = './logo.png'
 
 // 标题
-const title = 'Academic Project Page Template'
+const title = 'Future Lab for Artificial Intelligence and Robotics'
 
 // 标题颜色
 const title_color = '#000000'
