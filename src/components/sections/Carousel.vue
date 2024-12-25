86b852c1d60b0f9a28d5e8ea0b7c879b04169406
Henry.Liu <167392646+HenryLiukkk@users.noreply.github.com>
2024-12-25 15:14:10 +0800

    Update Carousel.vue

--- src/components/sections/Carousel.vue
+++ src/components/sections/Carousel.vue
@@ -25,7 +25,7 @@ export default {
         "./carousel/3.gif",
         "./carousel/4.gif",
         "./carousel/5.gif",
-        "./carousel/6.gif",
+        "./carousel/8.gif",
       ],
     }
   }
