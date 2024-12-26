597efcac7d3bb4c4a547affbdbe7b41230da5107
Henry.Liu <167392646+HenryLiukkk@users.noreply.github.com>
2024-12-26 13:04:38 +0800

    Update Video.vue

--- src/components/sections/Video.vue
+++ src/components/sections/Video.vue
@@ -14,7 +14,7 @@
     <el-row justify="center">
       <el-col :xs="24" :sm="20" :md="16" :lg="12" :xl="10" >
         <el-container id="video-container">
-          <iframe src="//player.bilibili.com/player.html?isOutside=true&aid=802517980&bvid=BV1qy4y1x7VV&cid=325180260&p=1" scrolling="no" border="0" frameborder="no" framespacing="0" allowfullscreen="true"></iframe>
+          <iframe src="https://www.bilibili.com/video/BV1W41fYbEup/?vd_source=cac3e2a4004d21a3918b58ae9386a694" scrolling="no" border="0" frameborder="no" framespacing="0" allowfullscreen="true"></iframe>
         </el-container>
       </el-col>
     </el-row>
